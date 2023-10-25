20a430d9f872c62b00915d2f33b47ca6e44c8865
Mohammad Ajaz Shaikh <erajaz24dec@gmail.com>
2023-10-25 21:21:45 +0530

    new Crud repo

---
+++ src/main/java/com/Employeeangularcrudapp/EmployeeAngularCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.Employeeangularcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAngularCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAngularCrudAppApplication.class, args);
+	}
+
+}
